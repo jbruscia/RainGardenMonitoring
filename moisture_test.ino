0718bdf5469345fdc48e5c716bdc4eba538dcb26
SusanBce2 <susan.nace.bce2@gmail.com>
2016-07-15 13:55:57 -0400

    added delay between sensor readings

--- moisture_test.ino
+++ moisture_test.ino
@@ -6,6 +6,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   double sensorValue1 = analogRead(A0);
+  delay(50);
   double sensorValue2 = analogRead(A1);
   
   
